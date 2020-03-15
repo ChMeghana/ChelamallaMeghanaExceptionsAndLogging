6df7f1b206d9cd595c5ab811a90f78a79e3a8927
Meghana <chelamallameghana2099@gmail.com>
2020-03-15 20:21:45 +0530

    Interests

---
+++ src/main/java/com/task_epam_5/Exceptions_Logger/SimpleInterest.java
@@ -0,0 +1,9 @@
+package com.task_epam_5.Exceptions_Logger;
+
+public class SimpleInterest {
+
+public double calculate(double principle,double time, double rate) {
+double simpleinterest = (principle * time * rate)/100;
+return simpleinterest;
+}
+}
